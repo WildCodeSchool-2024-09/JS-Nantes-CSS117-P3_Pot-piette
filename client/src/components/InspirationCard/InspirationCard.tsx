1b8d2044f0ebee0b3a20c6bfb60c60cb44eb1a16
vloiseau13 <valentin.loiseau13@gmail.com>
2025-01-10 14:40:31 +0100

    React fix

--- client/src/components/InspirationCard/InspirationCard.tsx
+++ client/src/components/InspirationCard/InspirationCard.tsx
@@ -2,12 +2,10 @@ import "./InspirationCard.css";
 
 function InspirationCard() {
   return (
-    <>
-      <figure className="inspiration-card">
-        <img src="" alt="" />
-        <figcaption>Hachis Parmentier</figcaption>
-      </figure>
-    </>
+    <figure className="inspiration-card">
+      <img src="" alt="" />
+      <figcaption>Hachis Parmentier</figcaption>
+    </figure>
   );
 }
 export default InspirationCard;
