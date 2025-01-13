2311fbcd1a7f1a797586ffb5fc05729cebbe8652
DamienD44 <damas44@gmail.com>
2025-01-13 17:08:09 +0100

    :construction:  Formulaire contact work in progress

---
+++ client/src/pages/FormulaireContact/FormulaireContact.tsx
@@ -0,0 +1,27 @@
+import "./FormulaireContact.css";
+
+function FormulaireContact() {
+  return (
+    <section className="contact-form-container">
+      <h2>Nous contacter :</h2>
+      <form className="contact-form">
+        <section className="form-group">
+          <input type="text" placeholder="Sujet ?" name="subject" />
+        </section>
+        <section className="contact-group">
+          <input type="text" placeholder="Nom / Prénom" name="name" />
+          <input type="email" placeholder="E-mail" name="email" />
+        </section>
+        <section className="contact-group">
+          <textarea placeholder="Remarque / Commentaire :" name="comment" />
+          <div className="character-count">/2000</div>
+        </section>
+        <button type="submit" className="submit-button">
+          Envoyer
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default FormulaireContact;
