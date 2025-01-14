51a57e4de09ffcea32efd54ccb5c8fcd4deb294d
MellerinCharly <mellerincharly.pro@gmail.com>
2025-01-14 17:12:04 +0100

    :building_construction: : Centring social media icons with link to their site. Responsive and desktop version available.

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -2,35 +2,67 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <>
-      <footer>
-        <ul className="icons-social-network">
-          <img
-            src="https://i.ibb.co/LtmJkt3/facebook.png"
-            alt="logo facebook"
-            className="social-network-icon"
-          />
-          <img
-            src="https://i.ibb.co/1QrM2Bx/instagram.png"
-            alt="logo instagram"
-            className="social-network-icon"
-          />
-          <img
-            src="https://i.ibb.co/SK4H7d6/twitter.png"
-            alt="logo Twitter"
-            className="social-network-icon"
-          />
-          <img
-            src="https://i.ibb.co/x1JckcH/pinterest.png"
-            alt="logo Pinterest"
-            className="social-network-icon"
-          />
-        </ul>
-        <p className="copyright-footer">
-          © 2025 Propulsé par la team Pot'Piette
-        </p>
-      </footer>
-    </>
+    <footer>
+      <ul className="icons-social-network">
+        <li>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Page Facebook"
+          >
+            <img
+              src="https://i.ibb.co/LtmJkt3/facebook.png"
+              alt="Logo Facebook"
+              className="social-network-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Page Instagram"
+          >
+            <img
+              src="https://i.ibb.co/1QrM2Bx/instagram.png"
+              alt="Logo Instagram"
+              className="social-network-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Compte Twitter"
+          >
+            <img
+              src="https://i.ibb.co/SK4H7d6/twitter.png"
+              alt="Logo Twitter"
+              className="social-network-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.pinterest.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Page Pinterest"
+          >
+            <img
+              src="https://i.ibb.co/x1JckcH/pinterest.png"
+              alt="Logo Pinterest"
+              className="social-network-icon"
+            />
+          </a>
+        </li>
+      </ul>
+      <p className="copyright-footer">© 2025 Propulsé par la team Pot'Piette</p>
+    </footer>
   );
 }
 
