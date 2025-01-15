5c892b5ccee6edf3ac54776b8a1038e32f9c8fbd
DamienD44 <damas44@gmail.com>
2025-01-15 15:08:04 +0100

    work still in progress css is on progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import AddRecipe5 from "./pages/AddRecipe/AddRecipe5";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <AddRecipe5 />
     </>
   );
 }
---
+++ client/src/pages/AddRecipe/AddRecipe5.tsx
@@ -0,0 +1,66 @@
+import "./AddRecipe5.css";
+
+function AddRecipe5() {
+  return (
+    <main className="add-5-recipe-main">
+      <section>
+        <h1>Grillée de mogette</h1>
+      </section>
+      <section className="add-5-recipe-ingrediens">
+        <h2>Ingrédients</h2>
+
+        <ul>
+          <button type="button" aria-label="Modifier le titre">
+            ✏️
+          </button>
+          <li>
+            <img src="src\assets\tests\Anchois.png" alt="Anchois" />
+            <span>Mogette</span>
+          </li>
+          <li>
+            <img src="src\assets\tests\Jambon.png" alt="Jambon" />
+            <span>Sel</span>
+          </li>
+          <li>
+            <button type="button" aria-label="Ajouter un ingrédient">
+              +
+            </button>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Préparation de la recette</h2>
+        <ul>
+          <li>
+            <span>Étape 1</span>
+            <button
+              type="button"
+              className="edit-button"
+              aria-label="Modifier l'étape"
+            >
+              ✏️
+            </button>
+            <button
+              type="button"
+              className="delete-button"
+              aria-label="Supprimer l'étape"
+            >
+              ❌
+            </button>
+          </li>
+          <li className="add-step">
+            <button type="button" aria-label="Ajouter une étape">
+              +
+            </button>
+          </li>
+        </ul>
+      </section>
+      <section className="recette-footer">
+        <button type="submit" className="submit-button">
+          Envoyer
+        </button>
+      </section>
+    </main>
+  );
+}
+export default AddRecipe5;
