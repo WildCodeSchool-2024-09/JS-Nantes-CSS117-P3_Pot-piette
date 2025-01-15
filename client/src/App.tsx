9b362989323d552d34553408fda316a9ee15adde
MellerinCharly <mellerincharly.pro@gmail.com>
2025-01-15 11:28:38 +0100

    removal of the DetailRecipePage route in app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,11 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import DetailRecipePage from "./pages/DetailRecipePage/DetailRecipePage";
 
 function App() {
   return (
     <>
       <Header />
-      <DetailRecipePage />
       <Footer />
       <Outlet />
     </>
