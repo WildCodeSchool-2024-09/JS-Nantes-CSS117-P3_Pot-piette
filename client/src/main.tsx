3606c1f41d63bbc2a5747261cdd6b7e5724bbade
DamienD44 <damas44@gmail.com>
2025-01-16 15:37:18 +0100

    css problem with span, work in progress

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,7 @@ import ConnexionPage from "./pages/ConnexionPage/ConnexionPage";
 import DetailRecipePage from "./pages/DetailRecipePage/DetailRecipePage";
 import Homepage from "./pages/Homepage/Homepage";
 import UserConnexion from "./pages/UserConnexion/UserConnexion";
+import ActivityPage from "./pages/ActivityPage/ActivityPage";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -47,6 +48,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <UserConnexion />,
+      },
+      {
+        path: "/My Activity",
+        element: <ActivityPage />,
       },
     ],
   },
--- client/src/pages/ActivityPage/ActivityPage.tsx
+++ client/src/pages/ActivityPage/ActivityPage.tsx
@@ -5,32 +5,30 @@ function ActivityPage() {
     <section className="activities">
       <h1>Vos activités</h1>
 
-      <section className="activitypublished-recipes">
+      <section>
         <h2>Mes recettes publiées</h2>
 
         <img
-          src="src/assets/test/Hachis Parmentier.jpg"
+          src="src/assets/tests/Hachis Parmentier.jpg"
           alt="Hachis Parmentier"
         />
 
         <p>Voir plus</p>
       </section>
 
-      <section className="activitywait-recipes">
+      <section className="waiting-recipe">
         <h2>Mes recettes en attente</h2>
 
         <img src="src\assets\tests\Anchois.png" alt="Anchois" />
 
         <img src="src\assets\tests\Jambon.png" alt="Jambon" />
 
-        <section>
-          <p>Voir plus</p>
-        </section>
+        <p>Voir plus</p>
       </section>
 
-      <section className="activityadd-recipe">
+      <section className="activity-add-recipe">
         <h2>Ajouter une recette</h2>
-        <span className="add-button">+</span>
+        <span className="activity-add-button">+</span>
       </section>
     </section>
   );
