ea376ee39545fd54be54aeacc8c3473c8c75d55f
ViriatoF <viriato.ferreira44@gmail.com>
2025-01-17 09:51:30 +0100

    🛂 Add interface for Recipe, Ingredient, step and tags

--- client/src/types/detail-recipe.d.ts
+++ client/src/types/detail-recipe.d.ts
@@ -1,5 +1,25 @@
-interface Recipe {
+export interface Recipe {
   title: string;
   picture: string;
   nb_parts: number;
+  time_to_cook: number;
+  preparation_time: number;
+  ingredient_list: IngredientI[];
+  recipe_steps: StepI[];
+  recipe_tag_list: TagI[];
+}
+
+interface IngredientI {
+  name: string;
+  measure: string;
+  quantity: number;
+}
+
+interface StepI {
+  content: string;
+  nb_step: number;
+}
+
+interface TagI {
+  tag_name: string;
 }
