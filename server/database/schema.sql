12cbf2e4e55676fe7ce0c79cfdfafcd5d91fff04
ViriatoF <viriato.ferreira44@gmail.com>
2025-01-17 14:42:19 +0100

    🔨 Fix bug schema.sql, comma to semicolon in preload data comment

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -66,7 +66,7 @@ VALUES
 ('Vito', 42, 'homme', 'vito.jpg', CURDATE(), 'viriato.ferreira44@gmail.com', 'password', 1, 1),
 ('Jean', 27, 'homme', 'jean.jpg', CURDATE(), 'jean@gmail.com', 'jaimelesucre', 0, 0),
 ('Mireille', 32, 'femme', 'mireille.jpg', CURDATE(), 'mireille@gmail.com', 'jaimepaslesucre', 0, 0),
-('Camille', 35, 'femme', 'camille.jpg', CURDATE(), 'camille@gmail.com', 'VeGanForEver', 0, 0),
+('Camille', 35, 'femme', 'camille.jpg', CURDATE(), 'camille@gmail.com', 'VeGanForEver', 0, 0);
 
 
 -- -----------------------------------------------------
