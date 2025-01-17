48d0440eee220217fc591402b81f70b42f42552b
ViriatoF <viriato.ferreira44@gmail.com>
2025-01-17 10:03:56 +0100

    :recycle: Change, separate and adapt interface for recipes

--- client/src/types/detail-recipe.d.ts
+++ client/src/types/detail-recipe.d.ts
@@ -1,9 +1,12 @@
 export interface Recipe {
   title: string;
   picture: string;
+  preparation_time: number;
+}
+
+export interface RecipeDetailI extends Recipe {
   nb_parts: number;
   time_to_cook: number;
-  preparation_time: number;
   ingredient_list: IngredientI[];
   recipe_steps: StepI[];
   recipe_tag_list: TagI[];
