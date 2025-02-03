ec915842d7defc3ab4de0e4b69b29fcd0652131a
ViriatoF <viriato.ferreira44@gmail.com>
2025-02-03 15:32:46 +0100

    Add schema.sql with user for dev tests

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -60,7 +60,12 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-
+INSERT INTO user (name, age, genre, picture, inscription_date, email, password, is_admin, is_modo)
+VALUES
+('Vito', 42, 'homme', 'vito.jpg', CURDATE(), 'viriato.ferreira44@gmail.com', 'password', 1, 1),
+('Jean', 27, 'homme', 'jean.jpg', CURDATE(), 'jean@gmail.com', 'jaimelesucre', 0, 0),
+('Mireille', 32, 'femme', 'mireille.jpg', CURDATE(), 'mireille@gmail.com', 'jaimepaslesucre', 0, 0),
+('Camille', 35, 'femme', 'camille.jpg', CURDATE(), 'camille@gmail.com', 'VeGanForEver', 0, 0);
 
 -- -----------------------------------------------------
 -- Table `potpiette`.`comment`
