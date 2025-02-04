e9ee20618c859d5ee8b26199e3a8406d5960daec
ViriatoF <viriato.ferreira44@gmail.com>
2025-02-04 11:49:29 +0100

    💄 Fix connexionPage css and add link to own pages

--- client/src/pages/ConnexionPage/ConnexionPage.tsx
+++ client/src/pages/ConnexionPage/ConnexionPage.tsx
@@ -1,18 +1,15 @@
+import { Link } from "react-router-dom";
 import "./ConnexionPage.css";
 
 function ConnexionPage() {
-  function handleLogin() {}
-
-  function handleSignup() {}
-
   return (
     <section className="button-container">
-      <button type="button" className="btn" onClick={handleLogin}>
-        Se connecter
-      </button>
-      <button type="button" className="btn" onClick={handleSignup}>
-        Créer un compte
-      </button>
+      <Link to={"/login"}>
+        <button type="button">Se connecter</button>
+      </Link>
+      <Link to={"/account"}>
+        <button type="button">Créer un compte</button>
+      </Link>
     </section>
   );
 }
