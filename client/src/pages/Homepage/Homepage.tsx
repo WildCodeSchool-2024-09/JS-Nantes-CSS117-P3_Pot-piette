eaf41917062e70093bb37d2e9701159c2eb2a109
DamienD44 <damas44@gmail.com>
2025-02-05 11:06:36 +0100

    Display search result after search bar

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -100,6 +100,16 @@ function Homepage() {
           </div>
         </form>
       </section>
+      <section className="search-container">
+        {recipes.map((recipe) => {
+          return (
+            <div key={recipe.id} className="search-result">
+              <img src={recipe.picture} alt={recipe.title} />
+              <figcaption>{recipe.title}</figcaption>
+            </div>
+          );
+        })}
+      </section>
 
       <h2>Inspirations</h2>
       <section className="home-inspirations">
@@ -180,17 +190,6 @@ function Homepage() {
         {filteredRecipes?.map((el) => (
           <InspirationCard key={el.id} picture={el.picture} title={el.title} />
         ))}
-      </section>
-
-      <section className="search-container">
-        {recipes.map((recipe) => {
-          return (
-            <div key={recipe.id} className="search-result">
-              <img src={recipe.picture} alt={recipe.title} />
-              <figcaption>{recipe.title}</figcaption>
-            </div>
-          );
-        })}
       </section>
     </main>
   );
