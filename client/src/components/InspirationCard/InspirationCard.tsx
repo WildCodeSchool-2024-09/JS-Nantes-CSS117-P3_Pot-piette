1b2a22a0f154bba67a2caa57db3015fdacf0ca5a
DamienD44 <damas44@gmail.com>
2025-02-05 16:13:15 +0100

    delete div from inspirationCard

--- client/src/components/InspirationCard/InspirationCard.tsx
+++ client/src/components/InspirationCard/InspirationCard.tsx
@@ -3,13 +3,11 @@ import "./InspirationCard.css";
 
 function InspirationCard({ picture, title }: RecipeI) {
   return (
-    <section className="inspiration-container">
-      <div className="inspiration-result">
-        <figure className="inspiration-card">
-          <img src={picture} alt={`Representation of ${title} card`} />
-          <figcaption>{title}</figcaption>
-        </figure>
-      </div>
+    <section className="inspiration-result">
+      <figure className="inspiration-card">
+        <img src={picture} alt={`Representation of ${title} card`} />
+        <figcaption>{title}</figcaption>
+      </figure>
     </section>
   );
 }
